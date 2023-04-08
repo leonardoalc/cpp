ddc25237acd88a648d948c2c59afe5350c38e54f
LeL-nardo <sandroleonardoads@gmail.com>
2023-04-08 16:16:05 -0300

    estrutura base

---
+++ 1_estrutura_base.cpp
@@ -0,0 +1,12 @@
+// bibliotecas a serem incluídas
+#include <iostream>
+
+using namespace std;
+
+// função principal
+int main() {
+    // código
+    cout << "Ola, mundo" << endl; // endl seria o \n
+    // retorno
+    return 0;
+}
