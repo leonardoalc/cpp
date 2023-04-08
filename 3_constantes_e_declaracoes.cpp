49c0e8d1d111b18875e9c74f786130b6f339ec7d
LeL-nardo <sandroleonardoads@gmail.com>
2023-04-08 17:29:39 -0300

    constantes e declarações múltiplas

---
+++ 3_constantes_e_declaracoes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// Declaração de constantes
+#define pi 3.14
+#define ola cout << "Ola, mundo! Sou uma constante." << endl;
+
+int main() {
+
+    int vidas = 3, municao;
+
+    int vidas2; int municao2 = 30;
+
+    cout << "Pi: " << pi << endl;
+    ola
+
+    return 0;
+}
