27835436386c4d07db7d685a8d70f089c3da6e26
LeL-nardo <sandroleonardoads@gmail.com>
2023-04-08 16:55:41 -0300

    variaveis e cin/cout

---
+++ 2_variaveis.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    // tipo nome;
+    // tipo nome = valor;
+
+    int num = 0; // 11, 7
+    char letra = 'S'; // 'A', 'b'
+    double numreal = 5.245667; // 3.14159265359
+    float numreal2 = 5.2; // 3.14
+    bool ligado = true; // true, false
+    string nome; // "Ola, mundo!", "Leonardo" 
+
+    cout << "Digite seu nome: ";
+    cin >> nome;
+    cout << nome;
+
+
+    return 0;
+}
