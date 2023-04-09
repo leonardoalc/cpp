6d6b18a3b36c99123639c6711a26fd20ef537b40
LeL-nardo <sandroleonardoads@gmail.com>
2023-04-09 15:57:57 -0300

    incremento e decremento

---
+++ 5_incremento_decremento.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n1, n2;
+
+    n1 = 0;
+    n2 = 11;
+
+    cout << n1 << "\n" << n2 << "\n\n";
+    n1++; // ++n1
+    n2 -= 3;
+    cout <<n1 << "\n" << n2 << "\n\n";
+
+    n1 *= 3;
+    n2 /= 2;
+    cout <<n1 << "\n" << n2;
+
+
+    return 0;
+}
