feacab50b175b32f6bd81f1743dc8c313848940a
LeL-nardo <sandroleonardoads@gmail.com>
2023-04-15 11:14:36 -0300

    break

---
+++ 10_while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+
+    n = 20;
+    while(n>=0) {
+        cout << n << "\n";
+        n--;
+    }
+
+    cout << "Rotina finalizada";
+
+    return 0;
+}
